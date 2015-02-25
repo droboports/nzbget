005a34cc6f2c6adef1e6402bd6bce2f5a4747b85
Ricardo Padilha <ricardospadilha@gmail.com>
2015-02-25 10:37:46 +0100

    Autoupdate support for DroboFS

--- src/update-STABLE.sh
+++ src/update-STABLE.sh
@@ -1,5 +1,5 @@
 _autoupdate() {
-local VERSION="14.2"
+local VERSION="14.2-fs"
 echo Downloading version ${VERSION}... >&3
 "${prog_dir}/libexec/wget" -O "${prog_dir}/../nzbget.tgz" "https://github.com/droboports/nzbget/releases/download/v${VERSION}/nzbget.tgz"
 setsid /bin/sh -c "echo Restarting NZBGet... >&3; /bin/sh ${prog_dir}/service.sh stop; sleep 5; /usr/bin/DroboApps.sh install" &
