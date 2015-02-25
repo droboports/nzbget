60c75e67d09af1a85f11cf47d17584da36f328b1
Ricardo Padilha <ricardospadilha@gmail.com>
2015-02-25 10:28:20 +0100

    Better logging for update-STABLE.sh

--- src/update-STABLE.sh
+++ src/update-STABLE.sh
@@ -1,6 +1,6 @@
 _autoupdate() {
 local VERSION="14.2"
-echo Downloading version ${VERSION}...
+echo Downloading version ${VERSION}... >&3
 "${prog_dir}/libexec/wget" -O "${prog_dir}/../nzbget.tgz" "https://github.com/droboports/nzbget/releases/download/v${VERSION}/nzbget.tgz"
-setsid /bin/sh -c "echo Restarting NZBGet...; /bin/sh ${prog_dir}/service.sh stop; sleep 5; /usr/bin/DroboApps.sh install" &
+setsid /bin/sh -c "echo Restarting NZBGet... >&3; /bin/sh ${prog_dir}/service.sh stop; sleep 5; /usr/bin/DroboApps.sh install" &
 }
