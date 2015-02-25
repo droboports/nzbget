8e746f5b1f6aa0e4faca5365ef3b41e388a8690d
Ricardo Padilha <ricardospadilha@gmail.com>
2015-02-25 10:19:03 +0100

    update-STABLE.sh with disown

--- src/update-STABLE.sh
+++ src/update-STABLE.sh
@@ -2,7 +2,12 @@ _autoupdate() {
 local VERSION="14.2"
 echo Downloading version ${VERSION}...
 "${prog_dir}/libexec/wget" -O "${prog_dir}/../nzbget.tgz" "https://github.com/droboports/nzbget/releases/download/v${VERSION}/nzbget.tgz"
-echo Restarting NZBGet...
-( /bin/sh "${prog_dir}/service.sh" stop; sleep 5; /usr/bin/DroboApps.sh install) &
-wait
+(
+  echo Restarting NZBGet...
+  sleep 1
+  /bin/sh "${prog_dir}/service.sh" stop
+  sleep 5
+  /usr/bin/DroboApps.sh install
+) &
+disown -h $!
 }
