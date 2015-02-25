92437616301222d43cbba66ebc5ee100c63b7b13
Ricardo Padilha <ricardospadilha@gmail.com>
2015-02-25 10:25:24 +0100

    update-STABLE.sh with setsid

--- src/update-STABLE.sh
+++ src/update-STABLE.sh
@@ -2,12 +2,5 @@ _autoupdate() {
 local VERSION="14.2"
 echo Downloading version ${VERSION}...
 "${prog_dir}/libexec/wget" -O "${prog_dir}/../nzbget.tgz" "https://github.com/droboports/nzbget/releases/download/v${VERSION}/nzbget.tgz"
-(
-  echo Restarting NZBGet...
-  sleep 1
-  /bin/sh "${prog_dir}/service.sh" stop
-  sleep 5
-  /usr/bin/DroboApps.sh install
-) &
-disown -h $!
+setsid /bin/sh -c "echo Restarting NZBGet...; /bin/sh ${prog_dir}/service.sh stop; sleep 5; /usr/bin/DroboApps.sh install" &
 }
